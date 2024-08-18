305be9266299912fe37c5717318666526a39d9f0
Tony Lam <tony.lam@iki.fi>
2024-08-18 14:04:56 +0300

    feat: add telegram link and make content max width

--- src/app/page.js
+++ src/app/page.js
@@ -22,32 +22,38 @@ export default function Home() {
     };
 
     return (
-        <div className={IBMFont.className}>
-            <h1 className={styles.headline}>Tony Lam</h1>
-            <h3 className={styles.headline}>
-                Computer Science Student at{" "}
-                <a
-                    href="https://www.helsinki.fi/en/faculty-science/faculty/computer-science"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    role="link"
-                >
-                    University of Helsinki
-                </a>
-            </h3>
-            <div className={styles.links}>
-                <CustomLink
-                    text={"LinkedIn"}
-                    link={"https://www.linkedin.com/in/lamtonylam"}
-                />
-                <CustomLink
-                    text={"GitHub"}
-                    link={"https://github.com/lamtonylam"}
-                />
-                <CustomLink
-                    text={"Email"}
-                    link={"mailto:tony@testausserveri.fi"}
-                />
+        <div className={styles.box}>
+            <div className={IBMFont.className}>
+                <h1 className={styles.headline}>Tony Lam</h1>
+                <h3 className={styles.headline}>
+                    Computer Science Student at{" "}
+                    <a
+                        href="https://www.helsinki.fi/en/faculty-science/faculty/computer-science"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        role="link"
+                    >
+                        University of Helsinki
+                    </a>
+                </h3>
+                <div className={styles.links}>
+                    <CustomLink
+                        text={"LinkedIn"}
+                        link={"https://www.linkedin.com/in/lamtonylam"}
+                    />
+                    <CustomLink
+                        text={"GitHub"}
+                        link={"https://github.com/lamtonylam"}
+                    />
+                    <CustomLink
+                        text={"Email"}
+                        link={"mailto:tony@testausserveri.fi"}
+                    />
+                    <CustomLink
+                        text={"Telegram"}
+                        link={"https://t.me/tonymaatti"}
+                    />
+                </div>
             </div>
         </div>
     );
