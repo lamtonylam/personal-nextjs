5175a425d75051ddbf50523601e02b65a8ce9017
Tony Lam <tony.lam@iki.fi>
2024-08-19 22:35:11 +0300

    feat: Add Simple Analytics script for page tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          defer
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
+        <noscript>
+          <img
+            src="https://queue.simpleanalyticscdn.com/noscript.gif"
+            alt=""
+            referrerpolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
+      </head>
       <body>{children}</body>
     </html>
   );
