d1fbb15a2d610b1c6a646a4c1caf11f3c2dc82e9
Tony Lam <tony.lam@iki.fi>
2025-02-18 10:04:11 +0200

    feat: update layout with max-width for better responsiveness and add Google site verification meta tag for mv.helsinki.fi

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -18,6 +18,10 @@ const Analytics = () => (
       src="https://umami.tonylam.iki.fi/script.js"
       data-website-id="fb46e08b-d710-443a-b8c2-dd28ccd99c31"
     ></script>
+    <meta
+      name="google-site-verification"
+      content="ApF6MwHd-TdfTrb9DJwu-0Q0kCuxefK3O133Du-rVQs"
+    />
   </>
 );
 
