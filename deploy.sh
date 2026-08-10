9966b6e7a0b252820a0ef31996f52e93a94290e4
Tony Lam <tony.lam@iki.fi>
2026-08-10 18:44:13 +0300

    fix: update deploy script rights

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,15 @@ cd build
 
 echo "Copying files to ssh"
 # copy files to the remote server
-sshpass -p "${PASSWORD}" scp -r ./* ${USERNAME}@melkki.cs.helsinki.fi:/cs/home/${USERNAME}/public_html
+sshpass -p "${PASSWORD}" scp -r ./* ${USERNAME}@melkinpaasi.cs.helsinki.fi:/cs/home/${USERNAME}/public_html
 
 echo "Change permissions"
 # change permissions of files on the remote server
-sshpass -p "${PASSWORD}" ssh "${USERNAME}@melkki.cs.helsinki.fi" 'chmod -R o+rx /cs/home/'"${USERNAME}"'/public_html'
+sshpass -p "${PASSWORD}" ssh "${USERNAME}@melkinpaasi.cs.helsinki.fi" "
+  chmod 755 /cs/home/${USERNAME}
+  find /cs/home/${USERNAME}/public_html -type d -exec chmod 755 {} +
+  find /cs/home/${USERNAME}/public_html -type f -exec chmod 644 {} +
+  rm -f /cs/home/${USERNAME}/public_html/trace
+"
 
 echo "Done. Exiting..."
